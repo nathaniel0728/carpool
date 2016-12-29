7b141a8b676ac8cfe136ddab809af53e07975c57
Nathaniel Choe <nchoe@ihmc.us>
2016-12-29 18:28:40 -0500

    initial configuration

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
+var exampleName = {fname:"john", lname:"smith"};
+var carpools = [exampleName];
+
+io.on("connection", function(socket){
+    
+    io.emit("carpool-update", carpools);
+    
+    socket.on("add-carpool", function(data){
+        carpools.push(data);
+        io.emit("carpool-update", carpools);
+    });
+
+});
+
 
 http.listen(5000, function(){
     console.log("listening on *:5000");
