033b49c145cfa7291c9272e695e69f9f479b22ad
nathaniel0728 <nchoe1999@gmail.com>
2017-01-25 02:40:07 +0000

    changed files

--- carpool.js
+++ carpool.js
@@ -7,16 +7,14 @@ module.exports = function Carpool(fn, ln, dsc, em, phn, l1, l2, d){
     this._phone = phn;
     this._loc = [l1, l2];
     this._days = d;
-    this._participants = {};
+    this._participants = [];
     this.fname = function(){
         return this._fname;
     }
     this.addParticipant = function(fname, lname, email, phn, d){
-        var info = [fname, lname, email, phn];
-        this._participants.push({
-            key: info,
-            value: d
-        })
+        //d = days available
+        var info = [fname, lname, email, phn, d];
+        this._participants.push(info)
     }
 }
 
--- server.js
+++ server.js
@@ -2,15 +2,40 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Carpool = require('./carpool');
+var MongoClient = require("mongodb").MongoClient;
+var assert = require("assert");
+
+var url = "mongodb://steven:stevenli@ds023694.mlab.com:23694/carpool-tjhsst"
+
+function insertData(fname, lname, desc, email, phone, loc1, loc2, days){
+    
+    MongoClient.connect(url, function(err, db){
+        assert.equal(null, err);
+        db.collection("carpools").insertOne({
+            "fname":fname,
+            "lname":lname,
+            "desc":desc,
+            "email":email,
+            "phone":phone,
+            "loc1":loc1,
+            "loc2":loc2,
+            "days":days
+        }, function(err, result){
+            assert.equal(err, null);
+            console.log("inserted");
+        });
+        db.close();
+    });
+
+}
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-var exampleName = new Carpool("Nathaniel", "Choe", "Sample Carpool", "nc@gmail.com", "703-222-2222", "Vienna",
+var exampleName = new Carpool("Steven", "Li", "Sample Carpool", "steve@gmail.com", "703-222-2222", "Vienna",
 "Alexandria", [0, 2, 1, 1, 1, 1, 1]);
 var carpools = [exampleName];
-//console.log(exampleName.fname());
 
 io.on("connection", function(socket){
     
@@ -19,8 +44,14 @@ io.on("connection", function(socket){
     socket.on("add-carpool", function(data){
         var cp = new Carpool(data.fname, data.lname, data.desc, data.email, data.phone, data.loc1, data.loc2, data.days); 
         carpools.push(cp);
+        insertData(data.fname, data.lname, data.desc, data.email, data.phone, data.loc1, data.loc2, data.days);
         io.emit("carpool-update", carpools);
     });
+
+    socket.on("add-participant", function(data){
+        
+    });
+
 
 });
 
